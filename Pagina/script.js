5e31f95d5a3dc238953294eb6d2f6299bbc50de3
Karenpaola20 <kbuelvas899@gmail.com>
2026-03-12 18:00:05 -0500

    first commit

---
+++ Pagina/script.js
@@ -0,0 +1,131 @@
+const pizzas = {
+
+    pepperoni:{
+        titulo:"Pizza Pepperoni",
+        imagen:"img/Pizza pepperoni.jpeg",
+        descripcion:"Deliciosa pizza con pepperoni, queso mozzarella y salsa de tomate.",
+
+        precios:{
+            pequena:"$45.000",
+            mediana:"$70.000",
+            familiar:"$80.000"
+        }
+    },
+
+    hawaiana:{
+        titulo:"Pizza Hawaiana",
+        imagen:"img/Pizza Hawaina.jpeg",
+        descripcion:"Pizza con jamón, piña y queso mozzarella.",
+
+        precios:{
+            pequena:"$35.000",
+            mediana:"$50.000",
+            familiar:"$70.000"
+        }
+    },
+
+    margarita:{
+        titulo:"Pizza Margarita",
+        imagen:"img/Pizza Margarita.jpeg",
+        descripcion:"Pizza con tomate, queso mozzarella y albahaca.",
+
+        precios:{
+            pequena:"$50.000",
+            mediana:"$70.000",
+            familiar:"$85.000"
+        }
+    },
+
+    polloChampi:{
+        titulo:"polloChampi",
+        imagen:"img/Pizza Pollo con champiñon.jpeg",
+        descripcion:"Pollo, salsa BBQ y queso mozzarella.",
+        precios:{
+            pequena:"$50.000",
+            mediana:"$75.000",
+            familiar:"$85.000"
+        }
+    },
+
+    polloBBQ:{
+        titulo:"Pizza Cuatro Quesos",
+        imagen:"img/Pizza de pollo salsa BBQ.jpg",
+        descripcion:"Mozzarella, parmesano, cheddar y queso azul.",
+        precios:{
+            pequena:"$70.000",
+            mediana:"$85.000",
+            familiar:"$100.000"
+        }
+    },
+
+    vegetariana:{
+        titulo:"Pizza Vegetariana",
+        imagen:"img/Pizzavegetariana.jpg",
+        descripcion:"Pimentón, champiñones, aceitunas y cebolla.",
+        precios:{
+            pequena:"$36.000",
+            mediana:"$52.000",
+            familiar:"$72.000"
+        }
+    }
+
+}
+
+function mostrarPizza(tipo){
+
+    const pizza = pizzas[tipo]
+
+    document.getElementById("tituloPizza").innerText = pizza.titulo
+    document.getElementById("imagenPizza").src = pizza.imagen
+    document.getElementById("descripcionPizza").innerText = pizza.descripcion
+    document.getElementById("precioPizza").innerHTML = `
+    <div class="opcion-precio">
+    <input type="radio" name="tamano" checked>
+    Pequeña (6 porciones) - ${pizza.precios.pequena}
+    </div>
+
+    <div class="opcion-precio">
+    <input type="radio" name="tamano">
+    Mediana (8 porciones) - ${pizza.precios.mediana}
+    </div>
+
+    <div class="opcion-precio">
+    <input type="radio" name="tamano">
+    Familiar (12 porciones) - ${pizza.precios.familiar}
+    </div>
+    `
+
+    document.getElementById("detallePizza").classList.remove("oculto")
+}
+
+function cerrarDetalle(){
+    document.getElementById("detallePizza").classList.add("oculto")
+}
+
+function animarPizzas(){
+
+    const pizzas = document.querySelectorAll(".pizza-card")
+
+    pizzas.forEach((pizza, index)=>{
+
+        setTimeout(()=>{
+
+            pizza.classList.add("animar")
+
+        }, index * 300)
+
+    })
+
+}
+
+window.addEventListener("load", function(){
+
+    setTimeout(function(){
+
+        document.getElementById("loader").style.display="none"
+
+        animarPizzas()
+
+    },2000)
+
+})
